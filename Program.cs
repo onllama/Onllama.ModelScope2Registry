3bae114b0b3296f53bfb4647fc74e8073917bcf3
Milkey Tan <24996957+mili-tan@users.noreply.github.com>
2025-01-10 00:17:43 +0800

    Update

--- Program.cs
+++ Program.cs
@@ -45,7 +45,7 @@ namespace Onllama.ModelScope2Registry
             });
 
             Parallel.ForEach(JsonNode.Parse(new HttpClient()
-                    .GetStringAsync("https://fastly.jsdelivr.net/gh/onllama/templates/index.json").Result)
+                    .GetStringAsync("https://raw.githubusercontent.com/onllama/templates/refs/heads/main/index.json").Result)
                 ?.AsArray() ?? [], i =>
             {
                 try
@@ -118,126 +118,138 @@ namespace Onllama.ModelScope2Registry
                 }
                 else
                 {
-                    await context.Response.WriteAsJsonAsync(new { error = "Blob not found" });
+                    await context.Response.WriteAsJsonAsync(new
+                        { errors = new { code = "BLOB_UNKNOWN", message = "Blob Unknown" } });
                     context.Response.StatusCode = 404;
                 }
             });
 
             app.Map("/v2/{user}/{repo}/manifests/{tag}", async context =>
             {
-                var user = context.Request.RouteValues["user"].ToString();
-                var repo = context.Request.RouteValues["repo"].ToString();
-                var tag = context.Request.RouteValues["tag"].ToString();
-
-                var templateTag = string.Empty;
-                if (tag != null && tag.Contains("--"))
-                {
-                    var sp = tag.Split("--");
-                    tag = sp.First();
-                    templateTag = sp.Last();
-                }
-
-                var modelScope = JsonSerializer.Deserialize<ModelScope>(
-                    await GetWithCache($"https://www.modelscope.cn/api/v1/models/{user}/{repo}/repo/files"));
-                var ggufFiles = modelScope.Data.Files.Where(x => x.Name.EndsWith(".gguf") && !x.Name.Contains("-of-"));
-                var ggufFile = tag != "latest"
-                    ? ggufFiles.FirstOrDefault(x =>
-                        x.Name.Split("-").Last().Split('.').First().ToUpper() == tag.ToUpper())
-                    : ggufFiles.FirstOrDefault(x =>
-                        x.Name.Split("-").Last().Split('.').First().ToUpper() is "Q4_K_M" or "Q4_0" or "Q8_0" );
-
-                if (ggufFile == null)
-                {
-                    await context.Response.WriteAsJsonAsync(new { error = "Model not found" });
-                    context.Response.StatusCode = 404;
-                    return;
-                }
-
-                var ggufDigest = $"sha256:{ggufFile.Sha256}";
-                RedirectDict.TryAdd(ggufDigest, $"https://www.modelscope.cn/models/{user}/{repo}/resolve/master/{ggufFile.Name}");
-                LenDict.TryAdd(ggufDigest,ggufFile.Size);
-                
-                var config = new object();
-                var layers = new List<object>
-                {
-                    new
-                    {
-                        mediaType = "application/vnd.ollama.image.model",
-                        size = ggufFile.Size,
-                        digest = ggufDigest
-                    }
-                };
-                
                 try
                 {
-                    var metadata = JsonNode.Parse(JsonNode.Parse(await PostWithCache(
-                            "https://www.modelscope.cn/api/v1/rm/fc?Type=model_view",
-                            JsonSerializer.Serialize(new
-                                {modelPath = user, modelName = repo, filePath = ggufFile.Name})))?
-                        ["Data"]?["metadata"]?.ToString() ?? string.Empty);
+                    var user = context.Request.RouteValues["user"].ToString();
+                    var repo = context.Request.RouteValues["repo"].ToString();
+                    var tag = context.Request.RouteValues["tag"].ToString();
 
-                    if (metadata != null)
+                    var templateTag = string.Empty;
+                    if (tag != null && tag.Contains("--"))
                     {
-                        var templateName = string.Empty;
-                        var configStr = modelConfig.Replace("<@MODEL>", metadata["general.architecture"]?.ToString() ?? string.Empty)
-                            .Replace("<@SIZE>", metadata["general.size_label"]?.ToString() ?? string.Empty);
-                        var configByte = Encoding.UTF8.GetBytes(configStr);
-                        var configDigest =
-                            $"sha256:{BitConverter.ToString(SHA256.HashData(configByte)).Replace("-", string.Empty).ToLower()}";
-                        var configSize = configByte.Length;
-                        DigestDict.TryAdd(configDigest, configStr);
+                        var sp = tag.Split("--");
+                        tag = sp.First();
+                        templateTag = sp.Last();
+                    }
 
-                        config = new
+                    var modelScope = JsonSerializer.Deserialize<ModelScope>(
+                        await GetWithCache($"https://www.modelscope.cn/api/v1/models/{user}/{repo}/repo/files"));
+                    var files = modelScope.Data.Files.Where(x => x.Name.EndsWith(".gguf") && !x.Name.Contains("-of-"));
+                    var gguf = tag != "latest"
+                        ? files.FirstOrDefault(x =>
+                            x.Name.Split("-").Last().Split('.').First().ToUpper() == tag.ToUpper())
+                        : files.OrderBy(x => x.Size).FirstOrDefault(x =>
+                            x.Name.Split("-").Last().Split('.').First().ToUpper() is "Q4_K_M" or "Q4_0" or "Q8_0");
+
+                    if (gguf == null)
+                    {
+                        await context.Response.WriteAsJsonAsync(new
+                        { errors = new { code = "MANIFEST_UNKNOWN", message = "Manifest Unknown" } });
+                        context.Response.StatusCode = 404;
+                        return;
+                    }
+
+                    var ggufDigest = $"sha256:{gguf.Sha256}";
+                    RedirectDict.TryAdd(ggufDigest, $"https://www.modelscope.cn/models/{user}/{repo}/resolve/master/{gguf.Name}");
+                    LenDict.TryAdd(ggufDigest, gguf.Size);
+
+                    var config = new object();
+                    var layers = new List<object>
+                    {
+                        new
                         {
-                            mediaType = "application/vnd.docker.container.image.v1+json",
-                            size = configSize,
-                            digest = configDigest
-                        };
+                            mediaType = "application/vnd.ollama.image.model",
+                            size = gguf.Size,
+                            digest = ggufDigest
+                        }
+                    };
 
-                        if (metadata["tokenizer.chat_template"] != null && TemplateMapDict.TryGetValue(
-                                metadata["tokenizer.chat_template"]?.ToString().Trim() ?? string.Empty, out templateName) || TemplateStrDict.ContainsKey(templateTag))
+                    try
+                    {
+                        var metadata = JsonNode.Parse(JsonNode.Parse(await PostWithCache(
+                                "https://www.modelscope.cn/api/v1/rm/fc?Type=model_view",
+                                JsonSerializer.Serialize(new
+                                { modelPath = user, modelName = repo, filePath = gguf.Name })))?
+                            ["Data"]?["metadata"]?.ToString() ?? string.Empty);
+
+                        if (metadata != null)
                         {
-                            templateName ??= templateTag;
-                            if (TemplateStrDict.TryGetValue(templateName, out var templateStr))
-                            {
-                                var templateByte = Encoding.UTF8.GetBytes(templateStr);
-                                var templateDigest = $"sha256:{BitConverter.ToString(SHA256.HashData(templateByte)).Replace("-", string.Empty).ToLower()}";
-                                DigestDict.TryAdd(templateDigest, templateStr);
-                                layers.Add(new
-                                {
-                                    mediaType = "application/vnd.ollama.image.template",
-                                    size = templateByte.Length,
-                                    digest = templateDigest
-                                });
-                            }
+                            var templateName = string.Empty;
+                            var configStr = modelConfig.Replace("<@MODEL>", metadata["general.architecture"]?.ToString() ?? string.Empty)
+                                .Replace("<@SIZE>", metadata["general.size_label"]?.ToString() ?? string.Empty);
+                            var configByte = Encoding.UTF8.GetBytes(configStr);
+                            var configDigest =
+                                $"sha256:{BitConverter.ToString(SHA256.HashData(configByte)).Replace("-", string.Empty).ToLower()}";
+                            var configSize = configByte.Length;
+                            DigestDict.TryAdd(configDigest, configStr);
 
-                            if (ParamsStrDict.TryGetValue(templateName, out var paramsStr))
+                            config = new
                             {
-                                var paramsByte = Encoding.UTF8.GetBytes(paramsStr);
-                                var paramsDigest = $"sha256:{BitConverter.ToString(SHA256.HashData(paramsByte)).Replace("-", string.Empty).ToLower()}";
-                                DigestDict.TryAdd(paramsDigest, paramsStr);
-                                layers.Add(new
+                                mediaType = "application/vnd.docker.container.image.v1+json",
+                                size = configSize,
+                                digest = configDigest
+                            };
+
+                            if (metadata["tokenizer.chat_template"] != null && TemplateMapDict.TryGetValue(
+                                    metadata["tokenizer.chat_template"]?.ToString().Trim() ?? string.Empty, out templateName) || TemplateStrDict.ContainsKey(templateTag))
+                            {
+                                templateName ??= templateTag;
+                                if (TemplateStrDict.TryGetValue(templateName, out var templateStr))
                                 {
-                                    mediaType = "application/vnd.ollama.image.params",
-                                    size = paramsByte.Length,
-                                    digest = paramsDigest
-                                });
+                                    var templateByte = Encoding.UTF8.GetBytes(templateStr);
+                                    var templateDigest = $"sha256:{BitConverter.ToString(SHA256.HashData(templateByte)).Replace("-", string.Empty).ToLower()}";
+                                    DigestDict.TryAdd(templateDigest, templateStr);
+                                    layers.Add(new
+                                    {
+                                        mediaType = "application/vnd.ollama.image.template",
+                                        size = templateByte.Length,
+                                        digest = templateDigest
+                                    });
+                                }
+
+                                if (ParamsStrDict.TryGetValue(templateName, out var paramsStr))
+                                {
+                                    var paramsByte = Encoding.UTF8.GetBytes(paramsStr);
+                                    var paramsDigest = $"sha256:{BitConverter.ToString(SHA256.HashData(paramsByte)).Replace("-", string.Empty).ToLower()}";
+                                    DigestDict.TryAdd(paramsDigest, paramsStr);
+                                    layers.Add(new
+                                    {
+                                        mediaType = "application/vnd.ollama.image.params",
+                                        size = paramsByte.Length,
+                                        digest = paramsDigest
+                                    });
+                                }
                             }
                         }
                     }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+
+                    await context.Response.WriteAsJsonAsync(new
+                    {
+                        schemaVersion = 2,
+                        mediaType = "application/vnd.docker.distribution.manifest.v2+json",
+                        config,
+                        layers
+                    });
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine(e);
+                    await context.Response.WriteAsJsonAsync(new
+                        { errors = new { code = "MANIFEST_UNKNOWN", message = e.Message } });
+                    context.Response.StatusCode = 404;
                 }
-
-                await context.Response.WriteAsJsonAsync(new
-                {
-                    schemaVersion = 2,
-                    mediaType = "application/vnd.docker.distribution.manifest.v2+json",
-                    config,
-                    layers
-                });
             });
 
             app.Run();
